8a9d784f5651bbf6114c8935cd6db1064f7ba7bf
MKrzek <mkrzek@googlemail.com>
2020-05-22 19:31:30 +0100

    feat: add routes to create and show post

--- BackendBlogApp/controllers/feed.js
+++ BackendBlogApp/controllers/feed.js
@@ -1,27 +1,74 @@
+const { validationResult } = require('express-validator');
+const Post = require('../models/post');
+
 const getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/duck.jpg',
-        creator: {
-          name: 'Maximilian',
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then(posts => {
+      console.log('posts', posts);
+      res.status(200).json({ posts });
+    })
+    .catch(err => {
+      console.log(err);
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const getSinglePost = (req, res, next) => {
+  const { postId } = req.params;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post!');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ post });
+    })
+    .catch(err => {
+      console.log(err);
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 const createPost = (req, res, next) => {
-  const { title, content } = req.body;
+  const errors = validationResult(req);
 
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title, content },
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect');
+    error.statusCode(422);
+    throw error;
+  }
+
+  const { title, content } = req.body;
+  const post = new Post({
+    title,
+    imageUrl: 'xxxxxx.jpg',
+    content,
+
+    creator: {
+      name: 'xxxx',
+    },
   });
+  post
+    .save()
+    .then(result => {
+      // Create post in db
+      console.log('result', result);
+      res.status(201).json({ post: result });
+    })
+    .catch(err => {
+      console.log(err);
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
-module.exports = { createPost, getPosts };
+
+module.exports = { createPost, getPosts, getSinglePost };
--- BackendBlogApp/routes/feed.js
+++ BackendBlogApp/routes/feed.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const { body } = require('express-validator');
 
-const { getPosts, createPost } = require('../controllers/feed');
+const { getPosts, createPost, getSinglePost } = require('../controllers/feed');
 
 const router = express.Router();
 
 // GET /feed/posts
 router.get('/posts', getPosts);
 
+router.get('/post/:postId', getSinglePost);
+
 // POST /feed/post,
-router.post('/post', createPost);
+router.post(
+  '/post',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 }),
+  ],
+  createPost
+);
 
 module.exports = router;
