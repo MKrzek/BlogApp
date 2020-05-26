6ca56414b4be2ae6f79cfea150c9bfd202c5c7a3
MKrzek <mkrzek@googlemail.com>
2020-05-26 12:55:41 +0100

    feat: added modal

---
+++ FrontentBlogApp/src/components/Modal/Modal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Button from '../Button/Button';
+import './Modal.css';
+
+const modal = ({title, children, onCancelModal, onAcceptModal, acceptEnabled, isLoading}) =>
+  ReactDOM.createPortal(
+    <div className="modal">
+      <header className="modal__header">
+        <h1>{title}</h1>
+      </header>
+      <div className="modal__content">{children}</div>
+      <div className="modal__actions">
+        <Button design="danger" mode="flat" onClick={onCancelModal}>
+          Cancel
+        </Button>
+        <Button
+          mode="raised"
+          onClick={onAcceptModal}
+          disabled={!acceptEnabled}
+          loading={isLoading}
+        >
+          Accept
+        </Button>
+      </div>
+    </div>,
+    document.getElementById('modal-root')
+  );
+
+export default modal;
