ad40c2daa1e57f10b1728341e2f8c85623d64c44
MKrzek <mkrzek@googlemail.com>
2020-05-26 12:37:39 +0100

    feat: add image component and styles

---
+++ FrontentBlogApp/src/components/Image/Image.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Image.css';
+
+const image = ({ imageUrl, contain, left }) => (
+  <div
+    className="image"
+    style={{
+      backgroundImage: `url('${imageUrl}')`,
+      backgroundSize: contain ? 'contain' : 'cover',
+      backgroundPosition: left ? 'left' : 'center'
+    }}
+  />
+);
+
+export default image;
