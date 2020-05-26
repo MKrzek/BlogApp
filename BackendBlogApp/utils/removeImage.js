8ab28264fd2976cc45a55d1a2e54bdb74c07ddbb
MKrzek <mkrzek@googlemail.com>
2020-05-26 14:33:08 +0100

    fix: fixed path in remove image util

--- BackendBlogApp/utils/removeImage.js
+++ BackendBlogApp/utils/removeImage.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const clearImage = filePath => {
-  const pathFile = path.join(__dirname, '../images', filePath);
+  const pathFile = path.join(__dirname, '..', filePath);
   fs.unlink(pathFile, err => console.log(err));
 };
 module.exports = clearImage;
