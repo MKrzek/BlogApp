bfb852c638954a3c9a3a84fe043a0506cb41c391
MKrzek <mkrzek@googlemail.com>
2020-05-26 12:45:22 +0100

    feat: add toolbar

---
+++ FrontentBlogApp/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Toolbar.css';
+
+const toolbar = ({ children }) => (
+    <div className="toolbar">
+       {children}
+    </div>
+);
+
+export default toolbar;
