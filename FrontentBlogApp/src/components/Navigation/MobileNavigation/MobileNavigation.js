f98490f4ecea790956acd7cc4741fea5fddeebe3
MKrzek <mkrzek@googlemail.com>
2020-05-26 12:51:02 +0100

    feat: add mobile navigation

---
+++ FrontentBlogApp/src/components/Navigation/MobileNavigation/MobileNavigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import NavigationItems from '../NavigationItems/NavigationItems';
+import './MobileNavigation.css';
+
+const mobileNavigation = ({open, mobile, onChooseItem, isAuth, onLogout}) => (
+  <nav className={['mobile-nav', open ? 'open' : ''].join(' ')}>
+    <ul
+      className={['mobile-nav__items', mobile ? 'mobile' : ''].join(' ')}
+    >
+      <NavigationItems
+        mobile
+        onChoose={onChooseItem}
+        isAuth={isAuth}
+        onLogout={onLogout}
+      />
+    </ul>
+  </nav>
+);
+
+export default mobileNavigation;
