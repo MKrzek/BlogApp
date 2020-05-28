5b9e59bf11c71f50e5a9333ebfc6dd1b53bafe2b
MKrzek <mkrzek@googlemail.com>
2020-05-28 16:42:23 +0100

    feat: add websocket to create/update/delete post handlers

--- BackendBlogApp/controllers/feed.js
+++ BackendBlogApp/controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const io = require('../socket');
 
 const Post = require('../models/post');
 const User = require('../models/user');
@@ -10,8 +11,11 @@ const getPosts = async (req, res, next) => {
   try {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
+      .populate('creator')
+      .sort({ createdAt: -1 })
       .skip((page - 1) * perPage)
       .limit(perPage);
+    console.log('postsss', posts);
     res.status(200).json({ posts, totalItems });
   } catch (err) {
     if (!err.statusCode) {
@@ -69,6 +73,10 @@ const createPost = async (req, res, next) => {
     user.posts.push(post);
     await user.save();
 
+    io.getIO().emit('posts', {
+      action: 'create',
+      post: { ...post._doc, creator: { _id: req.userId, name: user.name } },
+    });
     res.status(201).json({
       message: 'Post created successfully!',
       post,
@@ -105,16 +113,16 @@ const updatePost = async (req, res, next) => {
     throw error;
   }
   try {
-    const post = await Post.findById(postId);
+    const post = await Post.findById(postId).populate('creator');
 
     if (!post) {
       const error = new Error('Could not find post!');
       error.statusCode = 404;
       throw error;
     }
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error('Not authorized');
-      error.statusCode(403);
+      error.statusCode = 403;
       throw error;
     }
     if (imageUrl !== post.imageUrl) {
@@ -125,6 +133,11 @@ const updatePost = async (req, res, next) => {
     post.content = content;
 
     const result = await post.save();
+    console.log('post updated', result);
+    io.getIO().emit('posts', {
+      action: 'update',
+      post: result,
+    });
 
     res.status(200).json({ message: 'Post updated!', post: result });
   } catch (err) {
@@ -147,7 +160,7 @@ const deletePost = async (req, res, next) => {
     }
     if (post.creator.toString() !== req.userId) {
       const error = new Error('Not authorized');
-      error.statusCode(403);
+      error.statusCode = 403;
       throw error;
     }
     clearImage(post.imageUrl);
@@ -157,6 +170,8 @@ const deletePost = async (req, res, next) => {
 
     user.posts.pull(postId);
     await user.save();
+
+    io.getIO().emit('posts', { action: 'delete', post: postId });
 
     res.status(200).json({ message: 'Deleted post' });
   } catch (err) {
