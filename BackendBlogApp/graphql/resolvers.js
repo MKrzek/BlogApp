c398b0671d815b6f4744d7fddd4b2d4f8732d75d
MKrzek <mkrzek@googlemail.com>
2020-06-01 13:12:07 +0100

    feat: added schema and resolver for updating post

--- BackendBlogApp/graphql/resolvers.js
+++ BackendBlogApp/graphql/resolvers.js
@@ -148,7 +148,7 @@ module.exports = {
     }
     const post = await await Post.findById(id).populate('creator');
     if (!post) {
-      const error = new Error('No posts found');
+      const error = new Error('No post found');
       error.code = 404;
       throw error;
     }
@@ -158,6 +158,56 @@ module.exports = {
       _id: post._id.toString(),
       createdAt: post.createdAt.toISOString(),
       updatedAt: post.updatedAt.toISOString(),
+    };
+  },
+  updatePost: async ({ id, postInput }, req) => {
+    console.log('updattttttt', id, postInput);
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated');
+      error.code = 401;
+      throw error;
+    }
+    const post = await Post.findById(id).populate('creator');
+    console.log('found post', post);
+    if (!post) {
+      const error = new Error('No post found');
+      error.code = 404;
+      throw error;
+    }
+
+    if (post.creator._id.toString() !== req.userId.toString()) {
+      const error = new Error('Not authorized!');
+      error.code = 403;
+      throw error;
+    }
+    const { title, content, imageUrl } = postInput;
+    const errors = [];
+    if (validator.isEmpty(title) || !validator.isLength(title, { min: 5 })) {
+      errors.push({ message: 'Title is invalid' });
+    }
+    if (
+      validator.isEmpty(content) ||
+      !validator.isLength(content, { min: 5 })
+    ) {
+      errors.push({ message: 'Content is invalid' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    post.title = title;
+    post.content = content;
+    if (imageUrl !== 'undefined') {
+      post.imageUrl = imageUrl;
+    }
+    const updatedPost = await post.save();
+    return {
+      ...updatedPost._doc,
+      _id: updatedPost._id.toString(),
+      createdAt: updatedPost.createdAt.toISOString(),
+      updatedAt: updatedPost.updatedAt.toISOString(),
     };
   },
 };
