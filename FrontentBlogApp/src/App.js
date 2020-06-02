e3364a9e23a502645c09d6248ef3a304fbdc386f
MKrzek <mkrzek@googlemail.com>
2020-06-02 14:32:41 +0100

    fix: various fixes

--- FrontentBlogApp/src/App.js
+++ FrontentBlogApp/src/App.js
@@ -89,7 +89,7 @@ const App = (props) => {
     }
     try {
 
-      const res = await fetch('https://zeit.co/mkrzek/backend-blog-app/gmgwu5siy/graphql', {
+      const res = await fetch('https://backend-blog-app.mkrzek.now.sh/graphql', {
       method: 'POST',
       headers: {
         'Content-Type': "application/json"
@@ -154,7 +154,7 @@ const App = (props) => {
 
 
   try {
-    const res = await fetch('https://zeit.co/mkrzek/backend-blog-app/gmgwu5siy/graphql', {
+    const res = await fetch('https://backend-blog-app.mkrzek.now.sh/graphql', {
       method: 'POST',
       headers: { 'Content-Type': "application/json" },
       body: JSON.stringify(graphqlQuery )
